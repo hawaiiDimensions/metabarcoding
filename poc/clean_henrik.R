d894651f251aac765c915e26e47e4d82aab6b258
ajrominger <ajrominger@gmail.com>
2016-06-29 10:44:54 -0700

    got data load all worked out

--- poc/clean_henrik.R
+++ poc/clean_henrik.R
@@ -2,7 +2,9 @@ setwd('~/Dropbox/hawaiiDimensions/metabarcoding/poc')
 
 library(xlsx)
 
+## load data
 diffMarkers <- read.xlsx2('Blast_Results_DifferentMarkersPCRcycles200616.xlsx', sheetName='Blast_Results_DifferentMarkers2')
-diffMarkers <- read.xlsx2('Blast_Results_DifferentMarkersPCRcycles200616.xlsx', sheetName='Blast_Results_PCRcycleNumber200')
+pcrCycle <- read.xlsx2('Blast_Results_DifferentMarkersPCRcycles200616.xlsx', sheetName='Blast_Results_PCRcycleNumber200')
+
 
 ## DNA content of input is %DNA*15*124.5
