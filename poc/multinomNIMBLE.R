739e1692078e5aa4ff992e5593ecd95f02674919
ajrominger <ajrominger@gmail.com>
2016-08-29 13:29:26 -0700

    getting data organized for model

--- poc/multinomNIMBLE.R
+++ poc/multinomNIMBLE.R
@@ -1,9 +1,22 @@
 library(nimble)
+library(plyr)
+library(reshape2)
 
 setwd('~/Dropbox/hawaiiDimensions/metabarcoding/poc')
 
 ## read data for calibrating simulation
 diffMarkers <- read.csv('clean_diffMarkers.csv', as.is=TRUE)
+diffMarkers[diffMarkers$Specimen == diffMarkers$Specimen[1], c("Primer", "Pool", "number_Reads", "total_Reads", "amount_DNA")]
+
+head(melt(diffMarkers, c('Primer', 'Pool'), 'total_Reads'))
+
+totReads <- acast(melt(diffMarkers, c('Primer', 'Pool'), 'total_Reads'), Primer ~ Pool, 
+                  value.var =  'value', max, na.rm = TRUE, fill = 0)
+numReads <- acast(melt(diffMarkers, c('Primer', 'Pool', 'Specimen'), 'number_Reads'), Primer ~ Pool ~ Specimen, 
+                      value.var =  'value', max, na.rm = TRUE, fill = 0)
+## simpler than this!
+amountDNA <- acast(melt(diffMarkers, c('Primer', 'Pool', 'Specimen'), 'amount_DNA'), Primer ~ Pool ~ Specimen, 
+                   value.var =  'value', max, na.rm = TRUE, fill = 0)
 
 ## simulate data
 Npool <- 1000
@@ -11,7 +24,7 @@ Nreads <- Npool * 1000
 a <- 0.5
 amount_DNA <- runif(Npool, 1, 100)
 number_Reads <- as.vector(rmultinom(1, Nreads, rdirch(1, a*amount_DNA)))
-
+plot(amount_DNA, number_Reads)
 
 ## the model as NIMBLE code
 modCode <- nimbleCode({
@@ -53,4 +66,5 @@ par(mfrow = c(1, 2), mar = c(3, 2, 0, 1) + 0.1)
 plot(samp[-(1:burn), 'a'], type = 'l')
 abline(h = a, col = 'red')
 acf(samp[-(1:burn), 'a'])
-
+plot(density(samp[-(1:burn), 'a']))
+abline(v = a, col = 'red')
