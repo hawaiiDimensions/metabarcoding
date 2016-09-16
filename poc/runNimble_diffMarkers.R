96edcdc68bc2926a4831fc9fb38f77b056cfbc8f
ajrominger <ajrominger@gmail.com>
2016-09-16 13:57:57 -0700

    testing out mcmc params

--- poc/runNimble_diffMarkers.R
+++ poc/runNimble_diffMarkers.R
@@ -7,16 +7,23 @@ source('runNimble.R')
 
 diffMarkers <- read.csv('clean_diffMarkers.csv', as.is=TRUE)
 
+## number or reads for each primer and pool combination (rows:primers, cols:pools)
 totReads <- acast(melt(diffMarkers, c('Primer', 'Pool'), 'total_Reads'), Primer ~ Pool, 
                   value.var =  'value', max, na.rm = TRUE, fill = 0)
+
+## amount of DNA from each species in each pool (rows:pools, cols:species)
 amountDNA <- acast(melt(diffMarkers, c('Pool', 'Specimen'), 'amount_DNA'), Pool ~ Specimen, 
                    value.var =  'value', max, na.rm = TRUE, fill = 0)
+
+## number of reads per primer, pool, species combo (dim1:primer, dim2:pool, dim3:species)
 numReads <- acast(melt(diffMarkers, c('Primer', 'Pool', 'Specimen'), 'number_Reads'), Primer ~ Pool ~ Specimen, 
                   value.var =  'value', max, na.rm = TRUE, fill = 0)
 
-bla <- runNimble(totReads[8, ], amountDNA, numReads[8, , ])
+bla <- runNimble(totReads[8, ], amountDNA, numReads[8, , ], N = 10000, thin = 50, burn = 100)
 
-apply(numReads, 1, function(x) sum(x == 0) / prod(dim(x)))
+plot(bla[-(1:100), 1], type = 'l')
+plot(density(bla[-(1:100), 1]))
+acf(bla[-(1:100), 1])
 
-plot(bla[-(1:400), 1], ylim = range(bla[-(1:400), ]), type = 'l')
+plot(bla[-(1:500), 1], ylim = range(bla[-(1:400), ]), type = 'l')
 for(i in 2:ncol(bla)) lines(bla[-(1:400), i])
