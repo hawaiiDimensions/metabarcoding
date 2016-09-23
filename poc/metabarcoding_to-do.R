897e616ee0886f088f0e59da34b132ce9a89058b
ajrominger <ajrominger@gmail.com>
2016-09-23 16:35:51 -0700

    added to-do's for metabarcoding

---
+++ poc/metabarcoding_to-do.R
@@ -0,0 +1,5 @@
+## test which of the different markers is best with posterior predictive p-values
+
+## test for amplification bias by seeing if posteriors of different numbers of cycles are different
+
+## use cross validation to test if amount can be predicted from reads
