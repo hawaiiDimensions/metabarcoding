a50bb78e05c5aefd48487c61f5a12bcf766b9923
ajrominger <ajrominger@gmail.com>
2016-11-30 13:49:20 -0800

    removed stub for diffTissues, adding real tissue data and output

--- poc/nimble_run.R
+++ poc/nimble_run.R
@@ -5,12 +5,13 @@ setwd('~/Dropbox/hawaiiDimensions/metabarcoding/poc')
 source('nimble_funs.R')
 
 ## load data from different experiments
-diffMarkers <- read.csv('clean_diffMarkers.csv', as.is=TRUE)
-diffCycles <- read.csv('clean_pcrCycle.csv', as.is=TRUE)
+diffMarkers <- read.csv('clean_diffMarkers.csv', as.is = TRUE)
+diffCycles <- read.csv('clean_pcrCycle.csv', as.is = TRUE)
+diffTissue <- read.csv('clean_tissue.csv', as.is = TRUE)
 
 ## run nimble models
 diffMarkerOut <- buildNRunMod(diffMarkers, 'Primer')
 diffCyclesOut <- buildNRunMod(diffCycles, 'PCR_cycles')
-diffTissueOut <- NULL
+diffTissueOut <- buildNRunMod(diffTissue, 'Experiment')
 
 save(diffMarkerOut, diffCyclesOut, diffTissueOut, file = 'nimble_out.RData')
