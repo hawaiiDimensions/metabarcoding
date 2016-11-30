348a8242dc95f0a70d810f87145a7ed5cba4cead
ajrominger <ajrominger@gmail.com>
2016-11-29 17:12:48 -0800

    got script ready to run nimble models

--- poc/nimble_run.R
+++ poc/nimble_run.R
@@ -1,22 +1,16 @@
+## script to run nimble model on all experiments
+
+## load needed libraries and functions
 setwd('~/Dropbox/hawaiiDimensions/metabarcoding/poc')
 source('nimble_funs.R')
 
+## load data from different experiments
 diffMarkers <- read.csv('clean_diffMarkers.csv', as.is=TRUE)
 diffCycles <- read.csv('clean_pcrCycle.csv', as.is=TRUE)
-
-## number or reads for each primer and pool combination (rows:cycles, cols:pools)
-totReads <- acast(melt(diffCycles, c('PCR_cycles', 'Pool'), 'total_Reads'), PCR_cycles ~ Pool, 
-                  value.var =  'value', max, na.rm = TRUE, fill = 0)
-
-## amount of DNA from each species in each pool (rows:pools, cols:species)
-amountDNA <- acast(melt(diffCycles, c('Pool', 'Specimen'), 'amount_DNA'), Pool ~ Specimen, 
-                   value.var =  'value', max, na.rm = TRUE, fill = 0)
-
-## number of reads per primer, pool, species combo (dim1:cycles, dim2:pool, dim3:species)
-numReads <- acast(melt(diffCycles, c('PCR_cycles', 'Pool', 'Specimen'), 'number_Reads'), 
-                  PCR_cycles ~ Pool ~ Specimen, 
-                  value.var =  'value', max, na.rm = TRUE, fill = 0)
 
 ## run nimble models
 diffMarkerOut <- buildNRunMod(diffMarkers, 'Primer')
 diffCyclesOut <- buildNRunMod(diffCycles, 'PCR_cycles')
+diffTissueOut <- NULL
+
+save(diffMarkerOut, diffCyclesOut, diffTissueOut, file = 'nimble_out.RData')
