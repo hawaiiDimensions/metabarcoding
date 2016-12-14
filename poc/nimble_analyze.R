9ae84b2111e5a9275dfc661be78021ac34536410
ajrominger <ajrominger@gmail.com>
2016-12-14 09:31:33 -0800

    made R2 plot run from 0:1

--- poc/nimble_analyze.R
+++ poc/nimble_analyze.R
@@ -18,6 +18,10 @@ names(nuc) <- c('18sSSU', '18sM', '28sM', 'H3')
 mit <- c('12s_', 'CytB_', 'MCOHCO', 'ARFHCO')
 names(mit) <- c('12s', 'CytB', 'MCO', 'ARF')
 
+## ===========
+## plotting R2
+## ===========
+
 ## function to plot R2 values
 plotR2 <- function(x, ...) {
     r2mean <- x$R2.mean
@@ -29,12 +33,11 @@ plotR2 <- function(x, ...) {
          ...)
 }
 
-
-## plotting R2
+## plotting
 
 pdf('ms/fig_r2.pdf', width = 6, height = 4)
 par(mfrow = c(1, 4), oma = c(0, 3, 0, 0) + 0.5, mar = c(6, 0, 3, 0) + 0.5, cex.axis = 1.2)
-ylim <- c(0.5, 1)
+ylim <- c(0, 1)
 
 ## nuc
 plotR2(diffMarkerOut[[2]][match(nuc, diffMarkerOut[[2]]$Primer), ], cex = 1.5, 
@@ -66,3 +69,9 @@ mtext('Experiment', side = 1, line = 5)
 mtext('Tissue', side = 3, line = 1)
 
 dev.off()
+
+
+## ==============================
+## plotting corrrected read count
+## ==============================
+
